865678a6d2383d383f81145994eccb9b89de5936
rr_alves <rr.alves@campus.fct.unl.pt>
2017-10-29 13:54:56 +0000

    Pre populated database in ionic 3. Everythin in the most recent versions (node, npm, ionic, cordova, etc).

---
+++ prePopulatedDB/src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  private options = { name: "data.db", location: 'default', createFromLocation: 1 };
+  private queryNames = "SELECT * FROM testTable";
+  public names: String[] = [];
+
+  constructor(public navCtrl: NavController, private sqlite: SQLite) {
+    this.sqlite.create(this.options).then((db: SQLiteObject) => {
+      db.executeSql(this.queryNames, {}).then((data) => {
+        let rows = data.rows;
+        for (let i = 0; i < rows.length; i++)
+          this.names.push(rows.item(i).name);
+        console.log("Number of names on database = " + this.names.length);
+      })
+    });
+  }
+
+}
